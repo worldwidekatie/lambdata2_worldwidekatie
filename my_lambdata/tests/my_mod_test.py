dc754264fc024e13442b4d1690eb992ede9aeaf3
worldwidekatie <katie.evankodouglas@gmail.com>
2020-06-04 13:10:10 -0600

    added more tests

--- my_lambdata/tests/my_mod_test.py
+++ my_lambdata/tests/my_mod_test.py
@@ -1,18 +1,21 @@
 import unittest
 import pandas as pd
+import numpy as np
 from my_lambdata.my_mod import enlarge
 from my_lambdata.my_mod import t_test
+
+iris = 'my_lambdata/tests/iris.csv'
+ttest = 'my_lambdata/tests/t_test.csv'
 
 class TestMyMod(unittest.TestCase):
 
     def test_enlarge(self):
         self.assertEqual(enlarge(5), 500)
-    
-    df1 = pd.read_csv("iris.csv", names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class'])
-    df2 = pd.read_csv("t_test.csv")
 
     def test_t_test(self):
-        self.assertEqual(t_test(df1, 'class', 'Iris-setosa', 'Iris-virginica'), df2)
+        df = pd.read_csv(iris, names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class'])
+        df2 = pd.read_csv(ttest, index_col='Unnamed: 0')
+        self.assertEqual(t_test(df, 'class', 'Iris-setosa', 'Iris-virginica'), df2)
 
 if __name__ == '__main__':
     unittest.main()
